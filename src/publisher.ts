947e38c428bd448155dd02e9346195384399220f
Michael Wells <mwells1286@gmail.com>
2020-05-17 12:12:35 -0700

    reconfigure files and finish test.

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,18 +1,32 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
 console.clear();
 const stan = nats.connect('ticketing', 'abc', {
   url: 'https://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connect to NATS');
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 354,
-  });
 
-  stan.publish('ticket:created', data, () => {
-    console.log('event published', data);
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'book',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 354,
+  // });
+
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('event published', data);
+  // });
 });
